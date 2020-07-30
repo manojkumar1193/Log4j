89875d31e15fce0f1170ccee815a1bd4749d7862
manoj <manoj63sd@gmail.com>
2020-07-31 03:33:41 +0530

    Log4j

---
+++ Log4jTutorial/Log4jTutorial/src/beta/Demo1.java
@@ -0,0 +1,15 @@
+package beta;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Demo1 {
+	private static Logger log=LogManager.getLogger(Demo1.class.getName());
+	public static void main(String args[]) {
+		log.debug("I am debugging");
+		log.info("i will display information");
+		log.error("i will display error messages");
+		log.fatal("i will display fatal messages");
+	}
+
+}
